1adcbc7c57c1c64f588aab107dc8c55bb6b91d5b
Paul Hachmang <paul@h-o.nl>
2017-03-02 11:59:48 +0100

    #9 Solve issue where static content deploy didn't work

--- vagrant/provisioning/magento2.sh
+++ vagrant/provisioning/magento2.sh
@@ -10,3 +10,5 @@ $AS_USER touch ${HOME_DIR}/nginx/magento2.flag
 
 [ -d ${HOME_DIR}/${MAGENTO_DIR} ] || $AS_USER mkdir ${HOME_DIR}/${MAGENTO_DIR}
 [ -d ${HOME_DIR}/public ] || $AS_USER mkdir ${HOME_DIR}/public
+
+$AS_USER ln -fs ${HOME_DIR}/magento2/pub ${HOME_DIR}/public
