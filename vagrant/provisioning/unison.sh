08336dc2cea2101d033351b849208edce89108c1
Paul Hachmang <paul@h-o.nl>
2017-09-30 14:11:19 +0200

    Fix unison version

--- vagrant/provisioning/unison.sh
+++ vagrant/provisioning/unison.sh
@@ -9,11 +9,14 @@ apt-get install ocaml opam -y
 mkdir -p /usr/src/unison/
 cd /usr/src/unison/
 
-curl -L -o unison https://github.com/TentativeConvert/Syndicator/raw/master/unison-binaries/unison-2.48.3
-curl -L -o unison-fsmonitor https://github.com/TentativeConvert/Syndicator/raw/master/unison-binaries/unison-fsmonitor
+wget https://www.seas.upenn.edu/~bcpierce/unison/download/releases/unison-2.48.4/unison-2.48.4.tar.gz -O unison.tar.gz
+tar xzvf unison.tar.gz  --strip-components 1
+make UISTYLE=text || true
 
 chmod +x unison
 mv unison /usr/bin/
+
+curl -L -o unison-fsmonitor https://github.com/TentativeConvert/Syndicator/raw/master/unison-binaries/unison-fsmonitor
 
 chmod +x unison-fsmonitor
 mv unison-fsmonitor /usr/bin/
