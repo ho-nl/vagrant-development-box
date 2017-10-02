17a4ecd7875ff844444e588239493db9936f80ba
Paul Hachmang <paul@h-o.nl>
2017-10-02 14:15:04 +0200

    Fixede varnish config

--- vagrant/boot/varnish.sh
+++ vagrant/boot/varnish.sh
@@ -5,8 +5,13 @@ set -e
 AS_USER="sudo -u ${VAGRANT_USER}"
 HOME_DIR=$(getent passwd ${VAGRANT_USER} | cut -d ':' -f6)
 
-[ ${VARNISH_VCL} != false ] && varnishadm vcl.load mag2 ${HOME_DIR}/${VARNISH_VCL}
-[ ${VARNISH_VCL} != false ] && varnishadm vcl.use mag2
-[ ${VARNISH_VCL} != false ] && varnishadm "ban req.url ~ ."
-
 service varnish restart
+
+sleep 1
+
+if ! [ ${VARNISH_VCL} == 'false' ]; then
+    varnishadm vcl.load mag2 ${HOME_DIR}/${VARNISH_VCL}
+    varnishadm vcl.use mag2
+    varnishadm vcl.discard boot
+    varnishadm "ban req.url ~ ."
+fi
