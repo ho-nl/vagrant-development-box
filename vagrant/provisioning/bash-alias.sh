fc5d1dc29caf6ef21758dda9ef59b775775e9f1b
Hans Nieser <hans@reachdigital.nl>
2019-07-08 12:24:48 +0200

    Copy custom shell profile from host

--- vagrant/provisioning/bash-alias.sh
+++ vagrant/provisioning/bash-alias.sh
@@ -4,10 +4,19 @@ echo "🔥  Setting up bash aliases"
 
 CONFIG="
 alias phpd='php -dxdebug.remote_autostart=On'
+
 ";
 
 rm -f "${HOME_DIR}/.bash_aliases";
 touch "${HOME_DIR}/.bash_aliases";
 $AS_USER echo -n "${CONFIG}" > "${HOME_DIR}/.bash_aliases"
 
-echo $(cat ${HOME_DIR}/.bash_aliases)
+echo "$(cat ${HOME_DIR}/.bash_aliases)"
+
+if [ ! -z "${VAGRANT_HOST_CUSTOM_PROFILE}" ]
+then
+    echo "🔥  Setting up custom shell profile"
+    $AS_USER echo "${VAGRANT_HOST_CUSTOM_PROFILE}" > ${HOME_DIR}/.profile_custom
+
+    grep profile_custom ${HOME_DIR}/.bash_profile > /dev/null || echo "source ${HOME_DIR}/.profile_custom" >> ${HOME_DIR}/.bash_profile
+fi
