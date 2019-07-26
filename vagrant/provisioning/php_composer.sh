d8c9a55e0eb2c2aa0655075d1e7ba81b0c141170
Hans Nieser <hans@reachdigital.nl>
2019-07-26 10:42:05 +0200

    Remove PHP CLI memory limit

--- vagrant/provisioning/php_composer.sh
+++ vagrant/provisioning/php_composer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+PHP_VERSION=${VAGRANT_PHP_VERSION}
+
 echo "🔥  Ensuring latest version of composer is installed"
 
 set -e
 [ -f "/usr/local/bin/composer" ] || php -r "readfile('https://getcomposer.org/installer');" \
     | php -- --install-dir=/usr/local/bin --filename=composer \
     && /usr/local/bin/composer self-update
+
+# Remove PHP CLI memory limit to work around composer's extreme memory usage when calculating dependencies
+[ -f "/etc/php/${PHP_VERSION}/cli/conf.d/magweb.ini" ] && \
+    sed -i "s/memory_limit.*/memory_limit = -1/" "/etc/php/${PHP_VERSION}/cli/conf.d/magweb.ini"
